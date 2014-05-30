dcfa0b6aada34c80446c7c36dd8bc6b107a39124
faouellet <faouellet@gmail.com>
2014-05-29 20:21:32 -0400

    emitter finished

--- src/AssemblerLib/emitter.cpp
+++ src/AssemblerLib/emitter.cpp
@@ -9,13 +9,18 @@ Emitter::Emitter(const UInt32 & in_BufSize) :
 Emitter::~Emitter() { }
 
 void Emitter::EmitInstruction(const UInt8 in_Opcode, const UInt8 in_Op1, 
-        const UInt8 in_Op2, const UInt8 in_Op3)
+        const UInt8 in_Op2, const UInt16 in_Op3)
 {
     UInt32 l_Ins = in_Opcode;
-    l_Ins = l_Ins << 8 | in_Op2;
+    l_Ins = l_Ins << 4 | in_Op2;
     l_Ins = l_Ins << 4 | in_Op1;
-    l_Ins = l_Ins << 8 | in_Op3;
+    l_Ins = l_Ins << 16 | in_Op3;
     m_Buffer.push_back(l_Ins);
+}
+
+UInt16 Emitter::ReverseBytes(const UInt16 in_Bytes)
+{
+	return UInt16(((in_Bytes & 0xFF) << 8) | (in_Bytes >> 8));
 }
 
 void Emitter::EmitToFile(const std::string & in_Filename) const
@@ -28,166 +33,164 @@ void Emitter::EmitToFile(const std::string & in_Filename) const
 	}
 
     // TODO: Chip16 Header
-    
 }
 
 /////////////// Arithmetric ///////////////
 
-void Emitter::EmitAdd(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3)
+void Emitter::EmitAdd(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ)
 {
-    EmitInstruction(0x42, in_Op1, in_Op2, in_Op3);
+	EmitInstruction(0x42, in_RegX, in_RegY, UInt16(in_RegZ) << 8);
 }
 
-void Emitter::EmitAddImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitAddImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
 {
-    EmitInstruction(0x40, in_Op1, 0, in_Op2);
+	EmitInstruction(0x40, in_RegX, 0, ReverseBytes(in_ImmVal));
 }
 
-void Emitter::EmitAddInp(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitAddInp(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0x41, in_Op1, in_Op2, 0);
+	EmitInstruction(0x41, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitAnd(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3)
+void Emitter::EmitAnd(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ)
 {
-    EmitInstruction(0x62, in_Op1, in_Op2, in_Op3);
+	EmitInstruction(0x62, in_RegX, in_RegY, UInt16(in_RegZ) << 8);
 }
 
-void Emitter::EmitAndImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitAndImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
 {
-    EmitInstruction(0x60, in_Op1, 0, in_Op2);
+	EmitInstruction(0x60, in_RegX, 0, ReverseBytes(in_ImmVal));
 }
 
-void Emitter::EmitAndInp(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitAndInp(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0x61, in_Op1, in_Op2, 0);
+	EmitInstruction(0x61, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitCmp(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitARShift(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0x54, in_Op1, in_Op2, 0);
+	EmitInstruction(0xB5, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitARShift(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitARShiftImm(const UInt8 in_RegX, const UInt8 in_ImmVal)
 {
 
-    EmitInstruction(0xB5, in_Op1, in_Op2, 0);
+	EmitInstruction(0xB2, in_RegX, 0, UInt16(in_ImmVal) << 8);
 }
 
-void Emitter::EmitARShiftImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitCmp(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-
-    EmitInstruction(0xB2, in_Op1, 0, in_Op2);
+	EmitInstruction(0x54, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitCmpImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitCmpImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
 {
-    EmitInstruction(0x53, in_Op1, 0, in_Op2);
+	EmitInstruction(0x53, in_RegX, 0, ReverseBytes(in_ImmVal));
 }
 
-void Emitter::EmitDiv(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3)
+void Emitter::EmitDiv(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ)
 {
-    EmitInstruction(0xA2, in_Op1, in_Op2, in_Op3);
+	EmitInstruction(0xA2, in_RegX, in_RegY, UInt16(in_RegZ) << 8);
 }
 
-void Emitter::EmitDivImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitDivImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
 {
-    EmitInstruction(0xA0, in_Op1, 0, in_Op2);
+	EmitInstruction(0xA0, in_RegX, 0, ReverseBytes(in_ImmVal));
 }
 
-void Emitter::EmitDivInp(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitDivInp(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0xA1, in_Op1, in_Op2, 0);
+	EmitInstruction(0xA1, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitLLShift(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitLLShift(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0xB3, in_Op1, in_Op2, 0);
+	EmitInstruction(0xB3, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitLRShift(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitLLShiftImm(const UInt8 in_RegX, const UInt8 in_ImmVal)
 {
-    EmitInstruction(0xB4, in_Op1, in_Op2, 0);
+	EmitInstruction(0xB0, in_RegX, 0, UInt16(in_ImmVal) << 8);
 }
 
-void Emitter::EmitLLShiftImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitLRShift(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0xB0, in_Op1, 0, in_Op2);
+	EmitInstruction(0xB4, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitLRShiftImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitLRShiftImm(const UInt8 in_RegX, const UInt8 in_ImmVal)
 {
-    EmitInstruction(0xB1, in_Op1, 0, in_Op2);
+	EmitInstruction(0xB1, in_RegX, 0, UInt16(in_ImmVal) << 8);
 }
 
-void Emitter::EmitMul(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3)
+void Emitter::EmitMul(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ)
 {
-    EmitInstruction(0x92, in_Op1, in_Op2, in_Op3);
+	EmitInstruction(0x92, in_RegX, in_RegY, UInt16(in_RegZ) << 8);
 }
 
-void Emitter::EmitMulImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitMulImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
 {
-    EmitInstruction(0x90, in_Op1, 0, in_Op2);
+	EmitInstruction(0x90, in_RegX, 0, ReverseBytes(in_ImmVal));
 }
 
-void Emitter::EmitMulInp(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitMulInp(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0x91, in_Op1, in_Op2, 0);
+	EmitInstruction(0x91, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitOr(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3)
+void Emitter::EmitOr(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ)
 {
-    EmitInstruction(0x72, in_Op1, in_Op2, in_Op3);
+	EmitInstruction(0x72, in_RegX, in_RegY, UInt16(in_RegZ) << 8);
 }
 
-void Emitter::EmitOrImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitOrImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
 {
-    EmitInstruction(0x70, in_Op1, 0, in_Op2);
+	EmitInstruction(0x70, in_RegX, 0, ReverseBytes(in_ImmVal));
 }
 
-void Emitter::EmitOrInp(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitOrInp(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0x71, in_Op1, in_Op2, 0);
+	EmitInstruction(0x71, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitSub(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3)
+void Emitter::EmitSub(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ)
 {
-    EmitInstruction(0x52, in_Op1, in_Op2, in_Op3);
+	EmitInstruction(0x52, in_RegX, in_RegY, UInt16(in_RegZ) << 8);
 }
 
-void Emitter::EmitSubImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitSubImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
 {
-    EmitInstruction(0x50, in_Op1, 0, in_Op2);
+	EmitInstruction(0x50, in_RegX, 0, ReverseBytes(in_ImmVal));
 }
 
-void Emitter::EmitSubInp(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitSubInp(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0x51, in_Op1, in_Op2, 0);
+	EmitInstruction(0x51, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitTst(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitTst(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0x64, in_Op1, 0, in_Op2);
+	EmitInstruction(0x64, in_RegX, in_RegY, 0);
 }
 
-void Emitter::EmitTstImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitTstImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
 {
-    EmitInstruction(0x63, in_Op1, in_Op2, 0);
+	EmitInstruction(0x63, in_RegX, ReverseBytes(in_ImmVal), 0);
 }
 
-void Emitter::EmitXor(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3)
+void Emitter::EmitXor(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ)
 {
-    EmitInstruction(0x82, in_Op1, in_Op2, in_Op3);
+	EmitInstruction(0x82, in_RegX, in_RegY, UInt16(in_RegZ) << 8);
 }
 
-void Emitter::EmitXorImm(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitXorImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
 {
-    EmitInstruction(0x80, in_Op1, 0, in_Op2);
+	EmitInstruction(0x80, in_RegX, 0, ReverseBytes(in_ImmVal));
 }
 
-void Emitter::EmitXorInp(const UInt8 in_Op1, const UInt8 in_Op2)
+void Emitter::EmitXorInp(const UInt8 in_RegX, const UInt8 in_RegY)
 {
-    EmitInstruction(0x81, in_Op1, in_Op2, 0);
+	EmitInstruction(0x81, in_RegX, in_RegY, 0);
 }
 
 /////////////// Stack ///////////////
@@ -222,3 +225,171 @@ void Emitter::EmitPopF()
     EmitInstruction(0xC5, 0, 0, 0);
 }
 
+/////////////// Memory ///////////////
+void Emitter::EmitLDIImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x20, in_RegX, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitLDISP(const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x21, 0, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitLDM(const UInt8 in_RegX, const UInt8 in_RegY)
+{
+	EmitInstruction(0x23, in_RegX, in_RegY, 0);
+}
+
+void Emitter::EmitLDMImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x22, in_RegX, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitMov(const UInt8 in_RegX, const UInt8 in_RegY)
+{
+	EmitInstruction(0x24, in_RegX, in_RegY, 0);
+}
+
+void Emitter::EmitStm(const UInt8 in_RegX, const UInt8 in_RegY)
+{
+	EmitInstruction(0x31, in_RegX, in_RegY, 0);
+}
+
+void Emitter::EmitStmImm(const UInt8 in_RegX, const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x30, in_RegX, 0, ReverseBytes(in_ImmVal));
+}
+
+/////////////// Jump ///////////////
+void Emitter::EmitJmp(const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x10, 0, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitJx(const UInt8 in_RegX, const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x12, in_RegX, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitJme(const UInt8 in_RegX, const UInt8 in_RegY, const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x13, in_RegX, in_RegY, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitJmpInd(const UInt8 in_RegX)
+{
+	EmitInstruction(0x16, in_RegX, 0, 0);
+}
+
+void Emitter::EmitCall(const UInt8 in_RegX)
+{
+	EmitInstruction(0x18, in_RegX, 0, 0);
+}
+
+void Emitter::EmitCallImm(const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x14, 0, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitRet()
+{
+	EmitInstruction(0x15, 0, 0, 0);
+}
+
+void Emitter::EmitCx(const UInt8 in_RegX, const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x17, in_RegX, 0, ReverseBytes(in_ImmVal));
+}
+
+/////////////// Palette ///////////////
+void Emitter::EmitPal(const UInt8 in_RegX)
+{
+	EmitInstruction(0xD1, in_RegX, 0, 0);
+}
+
+void Emitter::EmitPalImm(const UInt16 in_ImmVal)
+{
+	EmitInstruction(0xD0, 0, 0, ReverseBytes(in_ImmVal));
+}
+
+/////////////// Sound ///////////////
+void Emitter::EmitSnd0()
+{
+	EmitInstruction(0x09, 0, 0, 0);
+}
+
+void Emitter::EmitSnd1(const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x0A, 0, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitSnd2(const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x0B, 0, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitSnd3(const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x0C, 0, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitSnp(const UInt8 in_RegX, const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x0C, in_RegX, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitSng(const UInt8 in_AD, const UInt8 in_SR, const UInt8 in_VT)
+{
+	// TODO: Maybe this won't work with EmitInstruction...
+	// EmitInstruction(0x0E, );
+}
+
+/////////////// Misc ///////////////
+void Emitter::EmitNop()
+{
+	EmitInstruction(0, 0, 0, 0);
+}
+
+void Emitter::EmitCls()
+{
+	EmitInstruction(0x01, 0, 0, 0);
+}
+
+void Emitter::EmitVBlnk()
+{
+	EmitInstruction(0x02, 0, 0, 0);
+}
+
+void Emitter::EmitBgc(const UInt8 in_Index)
+{
+	EmitInstruction(0x03, 0, 0, UInt16(in_Index) << 8);
+}
+
+void Emitter::EmitSpr(const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x04, 0, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitDrw(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ)
+{
+	EmitInstruction(0x06, in_RegX, in_RegY, UInt16(in_RegZ) << 8);
+}
+
+void Emitter::EmitDrwImm(const UInt8 in_RegX, const UInt8 in_RegY, const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x05, in_RegX, in_RegY, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitRnd(const UInt8 in_RegX, const UInt16 in_ImmVal)
+{
+	EmitInstruction(0x07, in_RegX, 0, ReverseBytes(in_ImmVal));
+}
+
+void Emitter::EmitFlip(const UInt8 in_X, const UInt8 in_Y)
+{
+	UInt16 l_Val = in_Y;
+	if (in_X)
+		l_Val += 2;
+	EmitInstruction(0x08, 0, 0, l_Val);
+}
--- src/AssemblerLib/emitter.h
+++ src/AssemblerLib/emitter.h
@@ -7,6 +7,7 @@
 #include "../Utils/utils.h"
 
 using Utils::UInt8;
+using Utils::UInt16;
 using Utils::UInt32;
 
 /*
@@ -32,55 +33,75 @@ public:
      */
     ~Emitter();
 
+	/*
+	* \fn EmitInstruction
+	* \brief Dump the Emitter instructions buffer in a file
+	* \param in_Filename Name of the file to writethe Chip16 instructions to
+	*/
     void EmitToFile(const std::string & in_Filename) const;
 
 private:
+	/*
+	* \fn EmitInstruction
+	* \brief Emits a Chip16 instruction in the Emitter internal buffer
+	* \param in_Opcode Opcode of the instruction
+	* \param in_Op1 First operand of the instruction
+	* \param in_Op1 Second operand of the instruction
+	* \param in_Op1 Third operand of the instruction (can be 16 bits immediate value)
+	*/
     void EmitInstruction(const UInt8 in_Opcode, const UInt8 in_Op1, 
-            const UInt8 in_Op2, const UInt8 in_Op3);
+            const UInt8 in_Op2, const UInt16 in_Op3);
+	/*
+	* \fn InverseBytes
+	* \brief Invert the two bytes making up an UInt16
+	* \param in_Bytes The two bytes to invert
+	* \return The inverted bytes
+	*/
+	UInt16 ReverseBytes(const UInt16 in_Bytes);
 
 public:    /////////////// Arithmetric ///////////////
-    void EmitAdd(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3);
-    void EmitAddImm(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitAddInp(const UInt8 in_Op1, const UInt8 in_Op2);
+    void EmitAdd(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ);
+    void EmitAddImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+    void EmitAddInp(const UInt8 in_RegX, const UInt8 in_RegY);
 
-    void EmitAnd(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3);
-    void EmitAndImm(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitAndInp(const UInt8 in_Op1, const UInt8 in_Op2);
+	void EmitAnd(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ);
+	void EmitAndImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitAndInp(const UInt8 in_RegX, const UInt8 in_RegY);
 
-    void EmitARShift(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitARShiftImm(const UInt8 in_Op1, const UInt8 in_Op2);
+    void EmitARShift(const UInt8 in_RegX, const UInt8 in_RegY);
+	void EmitARShiftImm(const UInt8 in_RegX, const UInt8 in_ImmVal);
     
-    void EmitCmp(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitCmpImm(const UInt8 in_Op1, const UInt8 in_Op2);
+	void EmitCmp(const UInt8 in_RegX, const UInt8 in_RegY);
+	void EmitCmpImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
 
-    void EmitDiv(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3);
-    void EmitDivImm(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitDivInp(const UInt8 in_Op1, const UInt8 in_Op2);
+	void EmitDiv(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ);
+	void EmitDivImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitDivInp(const UInt8 in_RegX, const UInt8 in_RegY);
 
-    void EmitLLShift(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitLLShiftImm(const UInt8 in_Op1, const UInt8 in_Op2);
+    void EmitLLShift(const UInt8 in_RegX, const UInt8 in_RegY);
+    void EmitLLShiftImm(const UInt8 in_RegX, const UInt8 in_ImmVal);
 
-    void EmitLRShift(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitLRShiftImm(const UInt8 in_Op1, const UInt8 in_Op2);
+    void EmitLRShift(const UInt8 in_RegX, const UInt8 in_RegY);
+    void EmitLRShiftImm(const UInt8 in_RegX, const UInt8 in_ImmVal);
     
-    void EmitMul(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3);
-    void EmitMulImm(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitMulInp(const UInt8 in_Op1, const UInt8 in_Op2);
+	void EmitMul(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ);
+	void EmitMulImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitMulInp(const UInt8 in_RegX, const UInt8 in_RegY);
 
-    void EmitOr(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3);
-    void EmitOrImm(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitOrInp(const UInt8 in_Op1, const UInt8 in_Op2);
+	void EmitOr(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ);
+	void EmitOrImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitOrInp(const UInt8 in_RegX, const UInt8 in_RegY);
 
-    void EmitSub(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3);
-    void EmitSubImm(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitSubInp(const UInt8 in_Op1, const UInt8 in_Op2);
+	void EmitSub(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ);
+	void EmitSubImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitSubInp(const UInt8 in_RegX, const UInt8 in_RegY);
 
-    void EmitTst(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitTstImm(const UInt8 in_Op1, const UInt8 in_Op2);
+	void EmitTst(const UInt8 in_RegX, const UInt8 in_RegY);
+    void EmitTstImm(const UInt8 in_Op1, const UInt16 in_ImmVal);
 
-    void EmitXor(const UInt8 in_Op1, const UInt8 in_Op2, const UInt8 in_Op3);
-    void EmitXorImm(const UInt8 in_Op1, const UInt8 in_Op2);
-    void EmitXorInp(const UInt8 in_Op1, const UInt8 in_Op2);
+	void EmitXor(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ);
+	void EmitXorImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitXorInp(const UInt8 in_RegX, const UInt8 in_RegY);
 
 public:    /////////////// Stack ///////////////
     void EmitPush(const UInt8 in_RegID);
@@ -89,6 +110,52 @@ public:    /////////////// Stack ///////////////
     void EmitPopAll();
     void EmitPushF();
     void EmitPopF();
+
+public:    /////////////// Memory ///////////////
+	void EmitLDIImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitLDISP(const UInt16 in_ImmVal);
+
+	void EmitLDM(const UInt8 in_RegX, const UInt8 in_RegY);
+	void EmitLDMImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+
+	void EmitMov(const UInt8 in_RegX, const UInt8 in_RegY);
+
+	void EmitStm(const UInt8 in_RegX, const UInt8 in_RegY);
+	void EmitStmImm(const UInt8 in_RegX, const UInt16 in_ImmVal);
+
+public:    /////////////// Jump ///////////////
+	void EmitJmp(const UInt16 in_ImmVal);
+	void EmitJx(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitJme(const UInt8 in_RegX, const UInt8 in_RegY, const UInt16 in_ImmVal);
+	void EmitJmpInd(const UInt8 in_RegX);
+
+	void EmitCall(const UInt8 in_RegX);
+	void EmitCallImm(const UInt16 in_ImmVal);
+	void EmitRet();
+	void EmitCx(const UInt8 in_RegX, const UInt16 in_ImmVal);
+
+public:    /////////////// Palette ///////////////
+	void EmitPal(const UInt8 in_RegX);
+	void EmitPalImm(const UInt16 in_ImmVal);
+
+public:    /////////////// Misc ///////////////
+	void EmitNop();
+	void EmitCls();
+	void EmitVBlnk();
+	void EmitBgc(const UInt8 in_Index);
+	void EmitSpr(const UInt16 in_ImmVal);
+	void EmitDrw(const UInt8 in_RegX, const UInt8 in_RegY, const UInt8 in_RegZ);
+	void EmitDrwImm(const UInt8 in_RegX, const UInt8 in_RegY, const UInt16 in_ImmVal);
+	void EmitRnd(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitFlip(const UInt8 in_X, const UInt8 in_Y);
+
+public:    /////////////// Sound ///////////////
+	void EmitSnd0();
+	void EmitSnd1(const UInt16 in_ImmVal);
+	void EmitSnd2(const UInt16 in_ImmVal);
+	void EmitSnd3(const UInt16 in_ImmVal);
+	void EmitSnp(const UInt8 in_RegX, const UInt16 in_ImmVal);
+	void EmitSng(const UInt8 in_AD, const UInt8 in_SR, const UInt8 in_VT);
 };
 
 #endif // ASM_EMITTER_H
